3d7c9cf79a6312aad4b9c1591a67f49bac83213b
FelixCourrede <116156131+FelixCourrede@users.noreply.github.com>
2023-02-13 09:46:05 +0100

    Implentation de l'objet Livre.js

---
+++ minilibrairie/src/Livre.js
@@ -0,0 +1,38 @@
+// une chose
+export default class Livre {
+    constructor(titre, quantité, prix) {
+      this._titre=titre;
+      this._quantité=quantité;
+      this._prix=prix;
+    }
+    get titre() {
+      return this._titre;
+    }
+    get quantité() {
+      return this.quantité;
+    }
+    get prix() {
+      return this.prix;
+    }
+  
+    // modifier fait / pas fait
+    ajouterStock() {
+      this._quantité = this._quantité+1;
+    }
+    // modifier le texte
+    supprimerStock() {
+      this._quantité = quantité-1;
+    }
+    afficherTitre(){
+        return`${this._titre}`;
+    }
+    afficherQuantité(){
+        return`${this._quantité}`;
+    }
+    afficherPrix(){ 
+       return`${this._prix}`;
+    }
+    pourAfficher() {
+        return `${this._titre} ${this._quantité} ${this._prix}`;
+    }
+  }
---
+++ minilibrairie/src/components/livre.vue
@@ -0,0 +1,11 @@
+<script>
+import Livre from '../Livre.js';
+let livre = new Livre('Livre Test', 1, 1);
+console.log(livre.pourAfficher());
+console.log(livre.afficherTitre())
+console.log(livre.afficherQuantité())
+console.log(livre.afficherPrix())
+</script>
+<template>
+    
+</template>
